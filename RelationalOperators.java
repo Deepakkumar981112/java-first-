3d1847df4e4f648539d3f158f30710e334ae32de
Deepakkumar981112 <deepakkumar98111206@gmail.com>
2025-06-16 12:15:41 +0530

    All relational operators

---
+++ RelationalOperators.java
@@ -0,0 +1,68 @@
+class greaterthan {
+    public static void main(String arr[]) {
+        int a = 5;
+        int b = 6;
+        boolean result = b > a;
+        System.out.println(result);
+    }
+}
+
+class smallerthan {
+    public static void main(String arr[]) {
+        int a = 5;
+        int b = 6;
+        boolean result = b < a;
+        System.out.println(result);
+    }
+}
+
+
+class notequalto {
+    public static void main(String arr[]) {
+        int a = 5;
+        int b = 6;
+        boolean result = b != a;
+        System.out.println(result);
+    }
+}
+
+
+class greaterthanEqualto {
+    public static void main(String arr[]) {
+        int a = 5;
+        int b = 6;
+        boolean result = b <= a;
+        System.out.println(result);
+    }
+}
+
+
+class smallerthanEqualto {
+    public static void main(String arr[]) {
+        int a = 5;
+        int b = 6;
+        boolean result = b >= a;
+        System.out.println(result);
+    }
+}
+
+
+class Equalto {
+    public static void main(String arr[]) {
+        double a =6 ;
+        int b = 5;
+        boolean result = b == a;
+        System.out.println(result);
+    }
+}
+
+class hre {
+    public static void main(String arr[]) {
+        double a =6 ;
+        int b = 5;
+        boolean result = b == a;
+        System.out.println(result);
+    }
+}
+
+
